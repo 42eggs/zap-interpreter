788ce25333a257286296beb8ebbfb8029b8f2aae
Nishan Das <21084024+42eggs@users.noreply.github.com>
2023-02-20 04:50:09 +0530

    can remember things as well as blocks

--- src/com/interpreter/zap/ASTPrinter.java
+++ src/com/interpreter/zap/ASTPrinter.java
@@ -5,6 +5,12 @@ class ASTPrinter implements Expr.Visitor<String> {
         return expr.accept(this);
     }
 
+
+    @Override
+    public String visitAssignExpr(Expr.Assign expr) {
+        return null;
+    }
+
     @Override
     public String visitBinaryExpr(Expr.Binary expr) {
         return parenthesize(expr.operator.lexeme,
@@ -26,6 +32,12 @@ class ASTPrinter implements Expr.Visitor<String> {
     public String visitUnaryExpr(Expr.Unary expr) {
         return parenthesize(expr.operator.lexeme, expr.right);
     }
+
+    @Override
+    public String visitVariableExpr(Expr.Variable expr) {
+        return null;
+    }
+
 
     private String parenthesize(String name, Expr... exprs) {
         StringBuilder builder = new StringBuilder();
