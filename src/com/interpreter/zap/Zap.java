0aa066af5351ee008c5c5b77c015fbbdd568461f
Nishan Das <21084024+42eggs@users.noreply.github.com>
2023-02-20 03:44:27 +0530

    Barebones interpreter

--- src/com/interpreter/zap/Zap.java
+++ src/com/interpreter/zap/Zap.java
@@ -10,7 +10,10 @@ import java.util.List;
 
 public class Zap {
 
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -26,6 +29,8 @@ public class Zap {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
+
     }
 
     private static void runPrompt() throws IOException {
@@ -45,10 +50,13 @@ public class Zap {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        // For now, just print the tokens.
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        // Stop if there was a syntax error.
+        if (hadError) return;
+
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -60,6 +68,20 @@ public class Zap {
         System.err.println(
                 "[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+                "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
 }
